9aa28b64565c6bdf528f5e728dac7b6c728d6471
rabb17 <156057471+rabb17chi@users.noreply.github.com>
2024-10-29 10:44:16 +0800

    Added Function Fare Checker

--- scripts/function/stationButtons.js
+++ scripts/function/stationButtons.js
@@ -4,25 +4,52 @@ import { bigData } from "../usefulData/stationName.js"
 import { lineData } from "../usefulData/lineName.js"
 import { calculateTimeDiff } from "../../script.js"
 import { lineColor } from "../usefulData/lineColor.js"
+import { stationFullName } from "../usefulData/stationFullName.js"
 
 const mapInfoContainer = document.getElementById('mapBtnInfoContainer')
-const abc = document.querySelectorAll('.station-btn') // get all buttons with class 'station-btn'
-let opened = false; // ContainerBox open/close 
+const circlesOptions = document.querySelectorAll('.station-btn')
+let modeSelection = document.querySelectorAll("input[name='MapFunction']")
+let mapFunction = 'checkTime' // map default function-> time checking
+let opened = false;
+
+let startStation = '';
+let endStation = '';
+
+let adultFare = ''
+let childFare = ''
+
+const CSV_URL = '../../resources/MTR_LINES-fares.csv'
+
+document.getElementById('resetButton').addEventListener('click',()=>{
+        startStation =''
+        endStation =''
+        console.log('Station selects are reset, please select 2 stations again.')
+        document.getElementById('priceHolder').style.display = 'none'
+        return false
+})
+
+modeSelection.forEach(item=>{
+    item.addEventListener('click',()=>{
+        mapFunction = item.value
+        document.getElementById('now-method').textContent = (mapFunction === 'checkTime' ? 'Check Train Time' : 'Check Price')
+        console.log('clicked, Mode is:', mapFunction)
+    })
+})
 
 function closeContainer() {
     mapInfoContainer.style.display ='none'
 mapInfoContainer.innerHTML=''
 }
 
-abc.forEach(item=>{ // change the color(s) of O
-    let styleArr = [] // Arr that being added to the O circle style
-    const stationLineArray = [...item.classList].filter(item=>item != 'station-btn') // get line array
+circlesOptions.forEach(item=>{ 
+    let styleArr = []
+    const stationLineArray = [...item.classList].filter(item=>item != 'station-btn') // get lines array
     const count = stationLineArray.length // number of lines that station has
     stationLineArray.map(item=> {
-        styleArr.push(lineColor[item]) // use "lineColor" with the key(item) to get the HEXcolor (eg. return #FFF)
+        styleArr.push(lineColor[item])
     })
 
-    if (count == 1) { // if the station has only 1 line, the 'O' will be using 1 color.
+    if (count == 1) {
         item.style['background'] = styleArr[0]
         return
     }
@@ -47,82 +74,115 @@ abc.forEach(item=>{ // change the color(s) of O
     }
 )
 
-abc.forEach(item=>{ // add fetch trains-data for every button O.
-    item.addEventListener('click',async (e)=>{
+async function loadCSV(url) {
+    try {
+        const res = await fetch(url);
+        let data = await res.blob();
+        Papa.parse(data, {
+            complete: result => {
+                getFares(result.data)
+            }
+        });
+    } catch (error) {
+        console.error('Message:',error)
+    }
+}
+function getFares(dataSet) {
+    dataSet.map( item =>{
+        if (item[0] === stationFullName[startStation] && item[2] === stationFullName[endStation]) {
+            console.log(`${bigData[startStation]} --> ${bigData[endStation]}\nAdult with Octopus', $${item[4]}; Child with Octopus, $${item[6]}`)
+            return item
+        }
+        return false
+    })
+}
+
+circlesOptions.forEach(item=>{
+    item.addEventListener('click',async () => {
+
         let msg = ''
         mapInfoContainer.innerHTML = ''
-        const BtnOfStationName = item.id // getting the name of station -> this will be using in the api (2nd para)
-        // console.log('Station:', bigData[BtnOfStationName]) // use "bigData" to get the chinese name of the station. (eg. 大圍) 
+        const BtnOfStationName = item.id
 
-        const BtnOfStationLine = [...item.classList].filter(item=>item != 'station-btn') // filter the classList, remove 'station-btn', this class is uselss in this block.
-        // console.log('BtnOfStationLine', BtnOfStationLine) // printing the classList, which should only show what line(s) the station has (eg. 觀塘線KTL/荃灣線TWL)
-        BtnOfStationLine.map(async(item,index)=>{ // loop every line to fetch trains-data
-            let url = `https://rt.data.gov.hk/v1/transport/mtr/getSchedule.php?line=${item}&sta=${BtnOfStationName}` // url from open-api, item(1st para) = line; StationName(2nd para) = station
-            const res = await fetch(url).then(res=>res.json()).then(data=>data.data)
-            const stationInfo = res[`${item}-${BtnOfStationName}`]  // getting the data that we need.('res' is object, so we have to [] also)
-            console.log(`(${lineData[item]}) ${BtnOfStationName} station`,stationInfo) // printing the station, UP-DOWN list and time.
-
-            // 終點站只有一個方向的車, 空白array保證輸出
-            const downList = stationInfo.DOWN || []
-            const upList = stationInfo.UP || [] 
-
-             // Need: [DEST](終點) and [time]
-            console.log('downList',downList ? downList : null)
-            console.log('upList',upList ? upList : null)
-
-            // 每條線 一個 section, 裡面包括: [線路-站名; 月台的最新4班列車狀況(終點站, 到站/下一班列車的時間)]
-            msg += `
-            <section style="display:flex">
-            <div class=${item}-${BtnOfStationName} id=${item}-${BtnOfStationName}-${index}
-                style="
-                width: 420px;
-                background-color: ${lineColor[item]}65;
-                "
-                >
-            <h2>${lineData[item]} - ${bigData[BtnOfStationName]}</h2>
-            <div style="display:flex;flex-direction:row;justify-content:space-around">
+        // Check Train Time function [1 button only]
+        if (mapFunction === 'checkTime') {
+            const BtnOfStationLine = [...item.classList].filter(item=>item != 'station-btn') // filter the classList, remove 'station-btn', this class is uselss in this block.
+            BtnOfStationLine.map(async(item,index)=>{ // loop every line to fetch trains-data
+                let url = `https://rt.data.gov.hk/v1/transport/mtr/getSchedule.php?line=${item}&sta=${BtnOfStationName}` // url from open-api, item(1st para) = line; StationName(2nd para) = station
+                const res = await fetch(url).then(res=>res.json()).then(data=>data.data)
+                const stationInfo = res[`${item}-${BtnOfStationName}`]  // getting the data that we need.('res' is object, so we have to [] also)
+                console.log(`(${lineData[item]}) ${BtnOfStationName} station`,stationInfo) // printing the station, UP-DOWN list and time.
+    
+                // 終點站只有一個方向的車, 空白array保證輸出
+                const downList = stationInfo.DOWN || []
+                const upList = stationInfo.UP || [] 
+    
+                // 每條線 一個 section, 裡面包括: [線路-站名; 月台的最新4班列車狀況(終點站, 到站/下一班列車的時間)]
+                msg += `
+                <section style="display:flex">
+                <div class=${item}-${BtnOfStationName} id=${item}-${BtnOfStationName}-${index}
+                    style="
+                    width: 420px;
+                    height: auto;
+                    background-color: ${lineColor[item]}65;
+                    "
+                    >
+                <h2>${lineData[item]} - ${bigData[BtnOfStationName]}</h2>
+                <div style="display:flex;flex-direction:row;justify-content:space-around">
+                    
+                    
+                    ${upList.length>0 ? `<span id='uplist-${item}'>` + upList.map((item,index)=>{
+                        if (index > 3) { // 部分車站提供4個以上的數據組, 為一致性便忽略這些資料
+                            return
+                        }
+                        let status;
+                        status = calculateTimeDiff(item.time, stationInfo[`curr_time`])
+                        return `<p style="width:210px;">終點站：<b>${bigData[item.dest]}</b> <br/> ${status}</p>`
+                    }) + "</span>":''
+                    }
+                    
                 
-                
-                ${upList.length>0 ? `<span id='uplist-${item}'>` + upList.map((item,index)=>{ // 即使Line47預防空白資料, 進一步使用array.length檢查是否有資料, 避免版面不符合預期.
+                ${downList.length>0 ? `<span id='downlist-${item}'>` + 
+                downList.map((item,index)=>{
                     if (index > 3) { // 部分車站提供4個以上的數據組, 為一致性便忽略這些資料
                         return
                     }
                     let status;
                     status = calculateTimeDiff(item.time, stationInfo[`curr_time`])
-                    return `<p style="width:210px;">終點站：<b>${bigData[item.dest]}</b> <br/> ${status}</p>`
-                }) + "</span>":''
+                    return `<p style="width:210px;">終點站：<b>${bigData[item.dest]}</b><br/> ${status} </p>`
+                }) + "</span>": ''
                 }
                 
-            
-            ${downList.length>0 ? `<span id='downlist-${item}'>` + 
-            downList.map((item,index)=>{ // 即使Line46預防空白資料, 進一步使用array.length檢查是否有資料, 避免版面不符合預期.
-                if (index > 3) { // 部分車站提供4個以上的數據組, 為一致性便忽略這些資料
-                    return
-                }
-                let status;
-                status = calculateTimeDiff(item.time, stationInfo[`curr_time`])
-                return `<p style="width:210px;">終點站：<b>${bigData[item.dest]}</b><br/> ${status} </p>`
-            }) + "</span>": ''
-            }
-            
-            </div>
-            </div>
-            </section>
-            <hr />
-            `
-            mapInfoContainer.style.display = 'block' // 由none改變成block
-            mapInfoContainer.innerHTML = msg.replaceAll(',','') // 減去不必要的符號
-            // document.getElementById(`${item}-${BtnOfStationName}-${index}`).style.background += `linear-gradient(to bottom, #fff 5%, ${lineColor[item]})`
-
+                </div>
+                </div>
+                </section>
+                <hr />
+                `
+                mapInfoContainer.style.display = 'block' 
+                mapInfoContainer.innerHTML = msg.replaceAll(',','')
+            })
+            opened = true
+            mapInfoContainer.style.display = 'block'
+            mapInfoContainer.style.overflowY = 'scroll'
+            setTimeout(()=>{ // timeout延遲 關閉按鈕 的出現, 避免fetch api過久而影響 關閉按鈕 的交互.
+                mapInfoContainer.innerHTML += `<span id='container-closeBtn' style='position:sticky;top:10%;color:red;'>X</span>`
+                document.getElementById('container-closeBtn').addEventListener('click',closeContainer)
+            },500)
+            return
+        }
+        
+        // Check Price function [2 buttons required]
+        if (startStation && endStation) {
+            console.log('please reset inputs first!')
+            return false
+        }
+        if (!startStation) {
+            console.log('1st',bigData[BtnOfStationName])
+            return startStation = BtnOfStationName
+        }
+        endStation = BtnOfStationName
+        console.log('2nd',bigData[BtnOfStationName])
+        loadCSV(CSV_URL)
+        document.getElementById('priceHolder').style.display = 'block'
         })
-        opened = true // container的狀況, 允許下面的function執行 關閉的工作 (Line10)
-        mapInfoContainer.style.display = 'block'
-        mapInfoContainer.style.overflowY = 'scroll'
-        setTimeout(()=>{ // timeout延遲 關閉按鈕 的出現, 避免fetch api過久而影響 關閉按鈕 的交互.
-            mapInfoContainer.innerHTML += `<span id='container-closeBtn' style='position:sticky;top:10%;color:red;'>X</span>`
-            document.getElementById('container-closeBtn').addEventListener('click',closeContainer)
-        },500)
-
-        })
-
 })
